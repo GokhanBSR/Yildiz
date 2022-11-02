71c98fe3c802b932e8e542ed2895258849578fa6
Gokhan Basara <gokhanbasara@Gokhan-MacBook-Pro-2.local>
2022-11-02 22:23:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+            for(int a=0; a<9; a++){
+                for(int b = 0; b<=a; b++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
+            }
+        }
+    }
